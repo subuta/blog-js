a1b0ad7d770230ae1d3cb3c49ee83e591f312418
subuta <shunta.3110@gmail.com>
2017-12-06 23:49:20 +0900

    Add koa server example.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('ppp');
+import Koa from 'koa'
+
+const app = new Koa();
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
+
+// response
+app.use(ctx => {
+  ctx.body = 'Hello aaa';
+});
+
+app.listen(3000);
