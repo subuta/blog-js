b2d485318af749023e9952352b418a68ef1eaa8d
subuta <shunta.3110@gmail.com>
2018-02-28 23:37:57 +0900

    Add requestChannels example.

--- src/views/pages/index.js
+++ src/views/pages/index.js
@@ -1,12 +1,12 @@
 import Layout from 'src/views/components/Layout'
 import Link from 'next/link'
-import axios from 'axios'
 import { styled } from 'react-free-style'
 import connext from 'src/views/hoc/connext'
 import Head from 'next/head'
 
 import {
-  requestChannels
+  requestChannels,
+  getAll as getChannels
 } from 'src/views/modules/channel'
 
 const withStyles = styled({
@@ -22,33 +22,30 @@ const Index = withStyles((props) => {
         <title>aa</title>
       </Head>
       <h1 onClick={props.increment}>Batman TV Shows({props.counter})</h1>
-      {/*
       <ul>
-        {props.shows.map(({show}) => (
-          <li key={show.id} className={props.styles.Link}>
-            <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
-              <a>{show.name}</a>
+        {props.channels.map(({id, name}) => (
+          <li key={id} className={props.styles.Link}>
+            <Link as={`/c/${id}`} href={`/channel?id=${id}`}>
+              <a>{name}</a>
             </Link>
           </li>
         ))}
       </ul>
-      */}
     </Layout>
   )
 })
 
-Index.getInitialProps = async function(ctx) {
-  // console.log(ctx.req.headers);
+Index.getInitialProps = async function (ctx) {
   await ctx.dispatch(requestChannels())
   return {}
 }
 
 const mapStateToProps = (state) => {
-  console.log(state);
-  return {}
+  return {
+    channels: getChannels(state)
+  }
 }
 
-const mapDispatchToProps = {
-}
+const mapDispatchToProps = {}
 
 export default connext(mapStateToProps, mapDispatchToProps)(Index)
