36aed3122f44523fa735d76f44896306337d681b
subuta <shunta.3110@gmail.com>
2018-03-02 00:16:08 +0900

    Add bundle size analyzer.

--- src/views/next.config.js
+++ src/views/next.config.js
@@ -1,6 +1,11 @@
 const webpack = require('webpack');
+const _ = require('lodash');
+const path = require('path');
+const { WebpackBundleSizeAnalyzerPlugin } = require('webpack-bundle-size-analyzer')
 
 require('dotenv').config({path: '../../.env'})
+
+const rootPath = path.resolve(process.cwd(), '../../')
 
 module.exports = {
   // https://gist.github.com/remy/6bb7beccc6355cafa7eac64f46467c66
@@ -11,6 +16,22 @@ module.exports = {
     }, {});
 
     config.plugins.push(new webpack.DefinePlugin(env));
+
+    // for bundle size check
+    // https://github.com/zeit/next.js/tree/master/examples/with-webpack-bundle-size-analyzer
+    if (process.env.ANALYZE) {
+      config.plugins.push(
+        new WebpackBundleSizeAnalyzerPlugin(path.resolve(rootPath, './webpack-stats.txt'))
+      )
+    }
+
+    // add resolve.root to project root.
+    const node_modules = [
+      ..._.get(config, 'resolve.modules', []),
+      rootPath
+    ]
+
+    config = _.set(config, 'resolve.modules', node_modules)
 
     return config;
   },
