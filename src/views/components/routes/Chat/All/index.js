f9a8a5a832d0702492c5f3ff7b2c65f3b4f59584
subuta <shunta.3110@gmail.com>
2018-03-04 23:45:06 +0900

    Add redirect.

--- src/views/components/routes/Chat/All/index.js
+++ src/views/components/routes/Chat/All/index.js
@@ -1,21 +1,29 @@
 import Layout from 'src/views/components/layout/Layout'
 import ChannelSidebar from 'src/views/components/routes/Chat/_Sidebar'
 import Content from 'src/views/components/layout/Content'
-import { withRouter } from 'next/router'
+import Router from 'next/router'
 import _ from 'lodash'
 
 import {
-  compose
+  compose,
+  lifecycle
 } from 'recompose'
 
 import connect from './connect'
 
 const enhance = compose(
-  withRouter,
-  connect
+  connect,
+  lifecycle({
+    // FIXME: renderの中でRouter.replaceを呼ぶと無限ループになる
+    componentWillMount () {
+      const {channels} = this.props
+      const channel = _.first(channels)
+      Router.replace(`/channel?id=${channel.id}`, `/c/${channel.id}`)
+    }
+  })
 )
 
-export default enhance(({router, channels}) => {
+export default enhance(({channels}) => {
   return (
     <Layout>
       <ChannelSidebar />
--- src/views/components/routes/Wiki/All/index.js
+++ src/views/components/routes/Wiki/All/index.js
@@ -1,10 +1,28 @@
 import Layout from 'src/views/components/layout/Layout'
 import WikiSidebar from 'src/views/components/routes/Wiki/_Sidebar'
 import Content from 'src/views/components/layout/Content'
+import Router from 'next/router'
+import _ from 'lodash'
+
+import {
+  compose,
+  lifecycle
+} from 'recompose'
 
 import connect from './connect'
 
-export default connect((props) => {
+const enhance = compose(
+  connect,
+  lifecycle({
+    componentWillMount () {
+      const {articles} = this.props
+      const article = _.first(articles)
+      Router.replace(`/article?id=${article.id}`, `/w/${article.id}`)
+    }
+  })
+)
+
+export default enhance((props) => {
   return (
     <Layout>
       <WikiSidebar />
