e305c8ee79ea5a47b50122262348b3694110de97
subuta <shunta.3110@gmail.com>
2018-03-25 18:55:04 +0900

    Fix table.

--- src/views/utils/markdown/table/transformer.js
+++ src/views/utils/markdown/table/transformer.js
@@ -1,39 +1,36 @@
 import visit from 'unist-util-visit'
-
+import find from 'unist-util-find'
 import _ from 'lodash'
 
-// FIXME: make code base compatible with rehype.
+const filter = (node, condition) => _.filter(node.children, condition)
+
 export default function transformer (settings = {}) {
   function visitor (node) {
-    if (node.type !== 'table') return
+    if (node.tagName !== 'table') return
 
-    let th = []
+    const thead = find(node, {tagName: 'thead'})
+    const ths = filter(find(thead, {tagName: 'tr'}), {tagName: 'th'})
+    const columns = _.map(ths, (th) => _.get(th, 'children.0.value'))
 
-    node.children.forEach((tr, i) => {
-      if (i === 0) {
-        th = tr
-        return
-      }
-
-      tr.children.forEach((td, i) => {
-        _.set(td, 'data', {
-          ..._.get(td, 'data', {}),
-          hName: 'td',
-          // wrap td child text as span for better styling.
-          hChildren: [{
-            type: 'element',
-            tagName: 'span',
-            properties: {},
-            children: td.children
-          }]
-        })
-        _.set(td, 'data.hProperties', {
-          ..._.get(td, 'data.hProperties', {}),
-          dataTh: _.get(th, ['children', i, 'children', 0, 'value'])
-        })
+    const tbody = find(node, {tagName: 'tbody'})
+    const trs = filter(tbody, {tagName: 'tr'})
+    _.each(trs, (tr) => {
+      _.each(filter(tr, {tagName: 'td'}), (td, i) => {
+        td.properties = {
+          ...td.properties,
+          dataTh: columns[i]
+        }
+        td.children = [{
+          type: 'element',
+          tagName: 'span',
+          properties: {},
+          children: td.children
+        }]
       })
     })
   }
 
-  return tree => visit(tree, 'table', visitor)
+  return tree => {
+    visit(tree, 'element', visitor)
+  }
 }
