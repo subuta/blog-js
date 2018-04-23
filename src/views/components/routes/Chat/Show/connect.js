80db1a6303aacfc0bbddb4522c0dcaf8f004a68d
subuta <shunta.3110@gmail.com>
2018-04-23 21:17:41 +0900

    Implement delete comment.

--- src/views/components/routes/Chat/Show/connect.js
+++ src/views/components/routes/Chat/Show/connect.js
@@ -1,11 +1,14 @@
 import _ from 'lodash'
 import { connect } from 'react-redux'
 import { createSelector } from 'reselect'
+import { normalize } from 'normalizr'
 
 import { getEntities as getChannelEntities } from 'src/views/modules/channel'
 import { denormalize } from 'src/views/utils/schema'
 
 import { throw404 } from 'src/views/utils/next'
+
+import { channel as channelSchema } from 'src/views/utils/schema'
 
 import {
   showMenu,
@@ -15,13 +18,14 @@ import {
 import {
   getAll as getChannels,
   getIsRequestProgress as getIsChannelProgress,
-  requestChannels
+  requestChannels,
+  setChannels
 } from 'src/views/modules/channel'
 
 import {
   createComment,
   updateComment,
-  deleteComment,
+  deleteComment as requestDeleteComment,
   getEntities as getCommentEntities
 } from 'src/views/modules/comment'
 
@@ -30,6 +34,27 @@ import {
   createAttachment,
   uploadAttachment
 } from 'src/views/modules/attachment'
+
+// optimistic delete comment.
+const deleteComment = (id, params) => {
+  return (dispatch, getState) => {
+    const state = getState()
+    const entities = getChannelEntities(state)
+    const channel = entities[params.channelId]
+    const _channel = _.clone(channel)
+
+    _channel.comments = _.without(channel.comments, id);
+
+    // optimistic delete comments
+    dispatch(setChannels(normalize(_channel, channelSchema)))
+
+    // then try actual deletion for the server
+    dispatch(requestDeleteComment(id, params)).catch(() => {
+      // rollback changes if failed.
+      dispatch(setChannels(normalize(channel, channelSchema)))
+    })
+  }
+}
 
 const getChannelComments = (name) => createSelector(
   getCommentEntities,
